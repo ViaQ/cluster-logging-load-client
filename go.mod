9b1b512fe093e7fde9422f3e1829830009aee933
Cyril Tovena <cyril.tovena@gmail.com>
2019-09-02 20:01:56 -0400

    simple logger

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module github.com/Kuqd/logger
 go 1.12
 
 require (
-	go.uber.org/atomic v1.4.0 // indirect
-	go.uber.org/multierr v1.1.0 // indirect
-	go.uber.org/zap v1.10.0
+	github.com/cortexproject/cortex v0.1.0-rc.0.0.20190825121948-934998160dbe
+	github.com/go-kit/kit v0.9.0 // indirect
+	github.com/gogo/googleapis v1.2.0 // indirect
+	github.com/gogo/status v1.1.0 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/grafana/loki v0.3.0
+	github.com/opentracing-contrib/go-stdlib v0.0.0-20190519235532-cf7a6c988dc9 // indirect
+	github.com/prometheus/common v0.4.1
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/weaveworks/common v0.0.0-20190822150010-afb9996716e4
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 )
--- main.go
+++ main.go
@@ -1,91 +1,134 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
-	"os"
-	"os/signal"
-	"strconv"
-	"sync"
-	"syscall"
+	"net/url"
 	"time"
 
-	"go.uber.org/zap"
+	"github.com/cortexproject/cortex/pkg/util"
+	"github.com/cortexproject/cortex/pkg/util/flagext"
+	"github.com/grafana/loki/pkg/promtail/client"
+	"github.com/prometheus/common/model"
+	"github.com/weaveworks/common/logging"
+	"github.com/weaveworks/common/server"
 )
 
-const defaultWorkerCount = 3
+func init() {
+	lvl := logging.Level{}
+	if err := lvl.Set("debug"); err != nil {
+		panic(err)
+	}
+	util.InitLogger(&server.Config{
+		LogLevel: lvl,
+	})
+}
 
 func main() {
-	cfg := zap.NewProductionConfig()
-	cfg.OutputPaths = []string{
-		"/var/log/myproject/myproject.log",
-	}
-	logger, err := cfg.Build()
+	u, err := url.Parse("http://localhost:3100/api/prom/push")
 	if err != nil {
 		panic(err)
 	}
-
-	defer logger.Sync()
-	var stop = make(chan os.Signal)
-	signal.Notify(stop, syscall.SIGTERM)
-	signal.Notify(stop, syscall.SIGINT)
-	workerCount := defaultWorkerCount
-	var wg sync.WaitGroup
-	wcount := os.Getenv("WORKER_COUNT")
-	if count, err := strconv.Atoi(wcount); err == nil {
-		workerCount = count
+	c, err := client.New(client.Config{
+		BatchWait: 0,
+		BatchSize: 100,
+		Timeout:   time.Second * 30,
+		BackoffConfig: util.BackoffConfig{
+			MinBackoff: time.Second * 1,
+			MaxBackoff: time.Second * 5,
+			MaxRetries: 5,
+		},
+		URL: flagext.URLValue{URL: u},
+	}, util.Logger)
+	if err != nil {
+		panic(err)
 	}
-	for i := 0; i < workerCount; i++ {
-		wg.Add(1)
-		go func() {
-			logLines(logger, stop)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-
-}
-
-func logLines(logger *zap.Logger, stop chan os.Signal) {
+	defer c.Stop()
 	for {
-		select {
-		case <-stop:
-			return
-		default:
-			randLevel(logger)(fmt.Sprintf("%d - %s", time.Now().UnixNano(), String(50)),
-				zap.String("component", "logger"),
-				zap.Int("attempt", 3),
-				zap.Duration("backoff", time.Second),
-			)
-			randLevel(logger)(fmt.Sprintf("%d - %s", time.Now().UnixNano(), String(50)))
-		}
+		_ = c.Handle(
+			model.LabelSet{
+				"service":   randService(),
+				"level":     randLevel(),
+				"component": randComponent(),
+			}, time.Now(), randomLog())
+		time.Sleep(time.Millisecond * 100)
 	}
+
 }
 
-const charset = "abcdefghijklmnopqrstuvwxyz" +
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-func StringWithCharset(length int, charset string) string {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[rand.Intn(len(charset)-1)]
-	}
-	return string(b)
+func randomLog() string {
+	return loglines[rand.Intn(len(loglines))]
 }
 
-func String(length int) string {
-	return StringWithCharset(length, charset)
+var loglines = []string{
+	"failing to cook potatoes",
+	"sucessfully launched a car in space",
+	"we got here",
+	"panic: could not read the manual",
+	"error while reading floppy disk",
+	"failed to reach the cloud, try again on a rainy day",
+	"failed to get an error message",
+	"You're screwed !",
+	"Oups I did it again",
+	"a chicken died during processing",
+	"sorry the server is not in a mood",
+	"Stupidity made this error, not me",
+	"random error happened during compression",
+	"too many foobar variable",
+	"cannot over-write a locked variable.",
+	"foo insists on strongly-typed programming languages",
+	"John Doe solved the Travelling Salesman problem in O(1) time. Here's the pseudo-code: Break salesman into N pieces. Kick each piece to a different city.",
+	"infinite loop succeeded in less than 3 seconds",
+	"could not compute the last digit of PI",
+	"OS not found try installing one",
+	"container sinked in whales",
+	"Don’t use beef stew as a computer password. It’s not stroganoff.",
+	"I used stack overflow to fix this bug",
+	"try googling this error message if it appears again",
+	"change stuff and see what happens",
+	"panic: this should never happen",
 }
 
-func randLevel(logger *zap.Logger) func(msg string, fields ...zap.Field) {
+func randLevel() model.LabelValue {
 	switch rand.Intn(3) {
 	case 0:
-		return logger.Info
+		return "info"
 	case 1:
-		return logger.Warn
+		return "warn"
 	case 2:
-		return logger.Debug
+		return "debug"
 	default:
-		return logger.Error
+		return "error"
+	}
+}
+
+func randComponent() model.LabelValue {
+	switch rand.Intn(4) {
+	case 0:
+		return "devopsend"
+	case 1:
+		return "fullstackend"
+	case 2:
+		return "frontend"
+	case 3:
+		return "everything-else"
+	default:
+		return "backend"
+	}
+}
+
+func randService() model.LabelValue {
+	switch rand.Intn(5) {
+	case 0:
+		return "potatoes-cart"
+	case 1:
+		return "phishing"
+	case 2:
+		return "stateless-database"
+	case 3:
+		return "random-policies-generator"
+	case 4:
+		return "cookie-jar"
+	default:
+		return "distributed-unicorn"
 	}
 }
